0b914516ee10cd59829ed1052b9986775da74101
Maryam Olaitan <151063603+Maryam1095@users.noreply.github.com>
2023-11-20 19:47:59 +0100

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -26,34 +26,34 @@ print("Available operation symbols:")
 for symbol in operations.keys():
     print(symbol)
 
-    # Prompt the user to enter the first number
-    num1 = float(input("Enter the first number: "))
-    # Prompt the user to enter the second number
-    num2 = float(input("Enter the second number: "))
+# Prompt the user to enter the first number
+num1 = float(input("Enter the first number: "))
+# Prompt the user to enter the second number
+num2 = float(input("Enter the second number: "))
 
-    while True:
+while True:
 
-        # Get the operation from the user
-        operation = input("Select an operation (+, -, *, /): ")
+    # Get the operation from the user
+    operation = input("Select an operation (+, -, *, /): ")
 
-        # Retrieve the corresponding function from the dictionary
-        calculation_function = operations[operation]
+    # Retrieve the corresponding function from the dictionary
+    calculation_function = operations[operation]
 
-        # Perform the calculation and store the result
-        answer = calculation_function(num1, num2)
+    # Perform the calculation and store the result
+    answer = calculation_function(num1, num2)
 
-        # Print the equation and the result
-        print(f"{num1} {operation} {num2} = {answer}")
+    # Print the equation and the result
+    print(f"{num1} {operation} {num2} = {answer}")
 
-        # Ask the user if they would like to continue using the result
-        choice = input("Continue using the result (Y/N)? ")
-        if choice.upper() == "Y":
-            # Update num1 with the result for further calculations
-            num1 = answer
-            num2 = float(input("Enter the second number: "))
+    # Ask the user if they would like to continue using the result
+    choice = input("Continue using the result (Y/N)? ")
+    if choice.upper() == "Y":
+        # Update num1 with the result for further calculations
+        num1 = answer
+        num2 = float(input("Enter the second number: "))
 
-        else:
-            # Prompt the user to enter the first number
-            num1 = float(input("Enter the first number: "))
-            # Prompt the user to enter the second number
-            num2 = float(input("Enter the second number: "))
+    else:
+        # Prompt the user to enter the first number
+        num1 = float(input("Enter the first number: "))
+        # Prompt the user to enter the second number
+        num2 = float(input("Enter the second number: "))
